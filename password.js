7b59a24deead00cf6c1869c902c7732b7564eb8a
Alec Faustino <alecfaustino@outlook.com>
2025-03-09 12:58:46 -0600

    refactored solve for password.js

--- password.js
+++ password.js
@@ -1,19 +1,15 @@
 const input = process.argv.slice(2); // creates an array with the args
 const inputStr = input[0];
 const obfuscate = (word) => {
+  const lettersToChange = {
+    a: "4",
+    e: "3",
+    o: "0",
+    l: "1",
+  }
   let obfuscated = ""
   for(const char of word) {
-    if (char === 'a') {
-      obfuscated += 4
-    } else if (char === 'e') {
-      obfuscated += 3
-    } else if (char === 'o') {
-      obfuscated += 0
-    } else if (char === 'l') {
-      obfuscated += 1
-    } else {
-      obfuscated += char;
-    }
+    obfuscated += lettersToChange[char] || char;
   }
   return obfuscated;
 };
